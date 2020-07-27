70e426d90e3007dc1a33ae579fd2c79461aeca64
Alberto Perez <aperez@lazer3p.local>
2020-07-26 21:02:46 -0700

    Updates

---
+++ precache-manifest.fc792cf2d3bf7e1e32253286a9f93395.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b739a7525805a616cbeccec5bed5d3db",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "171ee4dc3eeeff2419da",
+    "url": "/react-tic-tac-toe/static/css/main.3a40357d.chunk.css"
+  },
+  {
+    "revision": "642f11ce68d236595a80",
+    "url": "/react-tic-tac-toe/static/js/2.a2c56392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tic-tac-toe/static/js/2.a2c56392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "171ee4dc3eeeff2419da",
+    "url": "/react-tic-tac-toe/static/js/main.5df6a411.chunk.js"
+  },
+  {
+    "revision": "09962ff422a3e1e4ff6e",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.589ee1a9.js"
+  }
+]);
